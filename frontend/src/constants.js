ba04abeaefcb46f6d0b57680c3207ccd8f06628d
taikyunn <taichi_141@me.com>
2021-02-09 08:55:10 +0900

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,9 @@
 export const REQUEST_STATE = {
-  INTIAL:'INTIAL',
-  LOADING:'LOADING',
-  OK:'OK',
+  INITIAL: 'INITIAL',
+  LOADING: 'LOADING',
+  OK: 'OK',
 }
 
-export const HTTP_STATUS_CODE ={
-  NOT_ACCEPTABLE:406,
+export const HTTP_STATUS_CODE = {
+  NOT_ACCEPTABLE: 406,
 }
